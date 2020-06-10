173c126891676693c242a6d686fbbb5043785adb
Guyoguyes <g.abuba43@gmail.com>
2020-06-10 13:47:36 +0300

    foodtype getter and setters

--- src/main/java/models/FoodType.java
+++ src/main/java/models/FoodType.java
@@ -7,4 +7,30 @@ public class FoodType {
     public FoodType(String name){
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FoodType foodType = (FoodType) o;
+
+        if (id != foodType.id) return false;
+        return name != null ? name.equals(foodType.name) : foodType.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + id;
+        return result;
+    }
 }
