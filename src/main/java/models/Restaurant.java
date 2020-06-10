28b750cd5f084b6d416cfdd408a4a38645a1d97d
Guyoguyes <g.abuba43@gmail.com>
2020-06-10 13:33:31 +0300

    getter, setters, equals() and hashCode() added to restaurant class

--- src/main/java/models/Restaurant.java
+++ src/main/java/models/Restaurant.java
@@ -9,6 +9,14 @@ public class Restaurant {
     private String email;
     private int id;
 
+    public Restaurant(String name, String address, String zipcode, String phone, String email){
+        this.name = name;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.phone = phone;
+        this.email = email;
+    }
+
     public Restaurant(String name, String address, String zipcode, String phone, String website, String email){
         this.name = name;
         this.address = address;
@@ -18,11 +26,87 @@ public class Restaurant {
         this.email = email;
     }
 
-    public Restaurant(String name, String address, String zipcode, String phone, String email){
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Restaurant that = (Restaurant) o;
+
+        if (id != that.id) return false;
+        if (!name.equals(that.name)) return false;
+        if (!address.equals(that.address)) return false;
+        if (!zipcode.equals(that.zipcode)) return false;
+        if (!phone.equals(that.phone)) return false;
+        if (!website.equals(that.website)) return false;
+        return email.equals(that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + address.hashCode();
+        result = 31 * result + zipcode.hashCode();
+        result = 31 * result + phone.hashCode();
+        result = 31 * result + website.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + id;
+        return result;
     }
 }
